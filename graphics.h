5541ff476254ba5cea6445c5498efc06bb6f18e5
Kimia Shaban <shaban.kimia@gmail.com>
2020-04-19 16:42:16 -0400

    updated graphics.cc

--- graphics.h
+++ graphics.h
@@ -6,13 +6,19 @@
 #include "block.h"
 #include "state.h"
 
+/* GRAPHICS
+ * uses "window.h"
+ * align1 -> Player 1 graphics
+ * align2 -> Player 2 graphics 
+ */
+
 class Graphics {
-    // has a player
+    // HAS-A 2 players
     std::shared_ptr<Player> p1;
     std::shared_ptr<Player> p2;
     Xwindow xw;
     public:
-    Graphics();
+    Graphics(); // ctor
     void init(std::shared_ptr<Player> p1, std::shared_ptr<Player> p2, int score);
     void notify(State, int row, int col, char c);
     void blindnotify(State, int row, int col, char c);
@@ -20,10 +26,9 @@ class Graphics {
     void changeScore(int n);
     void drawBlind(State p);
     int getColour(char c);
-    void next(); // updates the next block
-    void NextBlockp1(char c);
+    void next(State p); // updates the next block
+    void NextBlock(State p, char c);
     void level6(State p);
-    void NextBlockp2(char c);
     Xwindow *getXwindow();
     void changeLevel();
     ~Graphics() = default;
