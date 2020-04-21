ee2bbaf0b7fbceaa1e46146e528944b12384ebc2
Jagrit <jchaitanya@hotmail.com>
2020-04-20 20:05:37 -0400

    made a command file with rename functionality

---
+++ command.cc
@@ -0,0 +1,265 @@
+#include "command.h"
+
+using namespace std;
+
+string Command::extractString(string n)
+{
+    string extract;
+    if(n.substr(0,4) == "nora" || n.substr(0,3) == "seq" || n.substr(0,6) == "rename") {
+        return n; 
+    }
+    // This will execute if it is not norandom or seq or rename
+    int len = n.length();
+    for (int i = 0; i < len; i++)
+    {
+        if (isalpha(n[i]))
+        {
+            n[i] = tolower(n[i]);
+            extract = extract + n[i];
+        }
+    }
+    return extract;
+}
+
+bool Command::checkinVector(string word, vector<string>& lines)
+{
+    // will go through and check the corresponding name
+    // will see if the file exists
+    int size = lines.size();
+    for (int i = 0; i < size; i++){
+        if (lines[i] == word){
+            return true;
+        }
+    }
+    return false;
+}
+
+int Command::getNum(string n)
+{
+    string nu = "";
+    int num;
+    int len = n.length();
+    for (int i = 0; i < len; i++)
+    {
+        if (isdigit(n[i]))
+        {
+            nu = nu + n[i];
+        }
+    }
+    if (nu == "")
+        return 0;
+    istringstream(nu) >> num;
+    return num;
+}
+
+static void readFile(vector<string>& v, string filename){
+    string line;
+    ifstream file(filename);
+	while (getline(file, line))
+	{
+		if(line.size() > 0)
+			v.emplace_back(line);
+	}
+}
+
+static bool replace(vector<string>& v, string old, string newName){
+    int size = v.size();
+    for (int i = 0; i < size; ++i){
+        if (v[i] == old){
+            v[i] = newName;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Command::isCoreCommand(){
+    if (this->oldName == "left") return true;
+    if (this->oldName == "right") return true;
+    if (this->oldName == "down") return true;
+    if (this->oldName == "clockwise") return true;
+    if (this->oldName == "counterclockwise") return true;
+    if (this->oldName == "drop") return true;
+    if (this->oldName == "levelup") return true;
+    if (this->oldName == "leveldown") return true;
+    if (this->oldName == "norandom") return true;
+    if (this->oldName == "random") return true;
+    if (this->oldName == "sequence") return true;
+    if (this->oldName == "restart") return true;
+    if (this->oldName == "rename") return true;
+    return false;
+}
+
+bool Command::rename(){
+    if (!validNewName()) {
+        cout << "Invalid rename, " << this->newName <<" already exists" << endl;
+        return false;
+    }
+    if (checkinVector(this->oldName, this->levelupVector)){
+        return replace(this->levelupVector, this->oldName, this->newName);
+    } else if (checkinVector(this->oldName, this->leveldownVector)){
+        return replace(this->leveldownVector, this->oldName, this->newName);
+    } else if (checkinVector(this->oldName, this->leftVector)){
+        return replace(this->leftVector, this->oldName, this->newName);
+    } else if (checkinVector(this->oldName, this->rightVector)){
+        return replace(this->rightVector, this->oldName, this->newName);
+    } else if (checkinVector(this->oldName, this->clockwiseVector)){
+        return replace(this->clockwiseVector, this->oldName, this->newName);
+    } else if (checkinVector(this->oldName, this->counterVector)){
+        return replace(this->counterVector, this->oldName, this->newName);
+    } else if (checkinVector(this->oldName, this->downVector)){
+        return replace(this->downVector, this->oldName, this->newName);
+    } else if (checkinVector(this->oldName, this->dropVector)){
+        return replace(this->dropVector, this->oldName, this->newName);
+    } else if (checkinVector(this->oldName, this->norandomVector)){
+        return replace(this->norandomVector, this->oldName, this->newName);
+    } else if (checkinVector(this->oldName, this->randomVector)){
+        return replace(this->randomVector, this->oldName, this->newName);
+    } else if (checkinVector(this->oldName, this->restartVector)){
+        return replace(this->restartVector, this->oldName, this->newName);
+    } else if (checkinVector(this->oldName, this->sequenceVector)){
+        return replace(this->sequenceVector, this->oldName, this->newName);
+    } 
+    return false;
+}
+
+void Command::readFiles(){
+    string line;
+    readFile(this->levelupVector, this->levelupfile);
+    readFile(this->leveldownVector, this->leveldownfile);
+    readFile(this->leftVector, this->leftfile);
+    readFile(this->rightVector, this->rightfile);
+    readFile(this->clockwiseVector, this->clockwisefile);
+    readFile(this->counterVector, this->counterfile);
+    readFile(this->downVector, this->downfile);
+    readFile(this->dropVector, this->dropfile);
+    readFile(this->norandomVector, this->norandomfile);
+    readFile(this->randomVector, this->randomfile);    
+    readFile(this->restartVector, this->restartfile);
+    readFile(this->sequenceVector, this->sequencefile);
+}
+
+string Command::getFile(){
+    return this->file;
+}
+
+void Command::setFile(string s){
+    this->file = s;
+}
+
+string Command::getOldName(){
+    return this->oldName;
+}
+string Command::getNewName(){
+    return this->newName;
+}
+
+bool Command::validNewName(){
+    if (checkinVector(this->newName, this->leftVector)) return false;
+    if (checkinVector(this->newName, this->rightVector)) return false;
+    if (checkinVector(this->newName, this->clockwiseVector)) return false;
+    if (checkinVector(this->newName, this->counterVector)) return false;
+    if (checkinVector(this->newName, this->downVector)) return false;
+    if (checkinVector(this->newName, this->dropVector)) return false;
+    if (checkinVector(this->newName, this->rightVector)) return false;
+    if (checkinVector(this->newName, this->levelupVector)) return false;
+    if (checkinVector(this->newName, this->leveldownVector)) return false;
+    if (checkinVector(this->newName, this->norandomVector)) return false;
+    if (checkinVector(this->newName, this->randomVector)) return false;
+    if (checkinVector(this->newName, this->restartVector)) return false;
+    if (checkinVector(this->newName, this->sequenceVector)) return false;
+    return true;
+}
+
+string Command::getCommand(string n)
+{
+    /* 
+     * used to obtain the commands 
+     * from the string constants 
+     * to ensure that short form and other
+     * forms of the commands can be used 
+     */
+    string cmd;
+   
+    cmd = extractString(n);
+    cout << cmd << endl;
+    if (checkinVector(cmd, this->leftVector))
+    {
+        return "left";
+    }
+    else if (checkinVector(cmd, this->rightVector))
+    {
+        return "right";
+    }
+    else if (checkinVector(cmd, this->downVector))
+    {
+        return "down";
+    }
+    else if (checkinVector(cmd, this->dropVector))
+    {
+        return "drop";
+    }
+    else if (checkinVector(cmd, this->clockwiseVector))
+    {
+        return "clockwise";
+    }
+    else if (checkinVector(cmd, this->counterVector))
+    {
+        return "counterclockwise";
+    }
+    else if (checkinVector(cmd, this->restartVector))
+    {
+        return "restart";
+    }
+    else if (checkinVector(cmd, this->randomVector))
+    {
+        return "random";
+    }
+    else if (checkinVector(cmd, this->levelupVector))
+    {
+        return "levelup";
+    }
+    else if (checkinVector(cmd, this->leveldownVector))
+    {
+        return "leveldown";
+    }
+    string firstcmd = "";
+    int i = 0;
+    int len = cmd.length();
+    for (; i < len; ++i){
+        if (cmd[i] == ' ') break;
+        firstcmd += cmd[i];
+    }
+
+    if (checkinVector(firstcmd, this->norandomVector)){
+        file = cmd.substr(i+1, cmd.length());
+        return "norandom";
+    }
+
+    else if (checkinVector(firstcmd, this->sequenceVector)){
+        file = cmd.substr(i+1, cmd.length());
+        return "sequence";
+    }
+    //if cmd is end, norand... or sequence return the whole string 
+    else if (cmd.substr(0,6) == "rename"){
+        this->oldName = "";
+        this->newName = "";
+        int j = 7;
+        int len = cmd.length();
+        for (; j < len; ++j){
+            if (cmd[j] == ' ') break;
+            this->oldName += cmd[j];
+        }
+        ++j;
+
+        for (;j < len; ++j){
+            this->newName += cmd[j];
+        }
+        return "rename";
+    }
+    else
+    {
+        return cmd;
+    }
+}
+
---
+++ command.h
@@ -0,0 +1,54 @@
+#ifndef _COMMAND_
+#define _COMMAND_
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+class Command{
+    std::string levelupfile = "textFiles/possible_levelu.txt";
+    std::string leveldownfile = "textFiles/possible_leveld.txt";
+    std::string leftfile = "textFiles/possible_left.txt";
+    std::string rightfile = "textFiles/possible_right.txt";
+    std::string downfile = "textFiles/possible_down.txt";
+    std::string dropfile = "textFiles/possible_drop.txt";
+    std::string clockwisefile = "textFiles/possible_clockwise.txt";
+    std::string counterfile = "textFiles/possible_counter.txt";
+    std::string restartfile = "textFiles/possible_restart.txt";
+    std::string hintfile = "textFiles/possible_hint.txt";
+    std::string norandomfile = "textFiles/possible_norandom.txt";
+    std::string randomfile = "textFiles/possible_random.txt";
+    std::string sequencefile = "textFiles/possible_sequence.txt";
+    std::vector <std::string> clockwiseVector;
+    std::vector <std::string> counterVector;
+    std::vector <std::string> downVector;
+    std::vector <std::string> dropVector;   
+    std::vector <std::string> leftVector;
+    std::vector <std::string> levelupVector;
+    std::vector <std::string> leveldownVector;
+    std::vector <std::string> norandomVector;
+    std::vector <std::string> randomVector;
+    std::vector <std::string> restartVector;
+    std::vector <std::string> rightVector;
+    std::vector <std::string> sequenceVector;
+    std::string file;
+    std::string oldName = ""; // for rename
+    std::string newName = ""; // for rename
+    public:
+        std::vector <std::string> seq_comm; 
+        bool readSeq = false;
+        std::string getCommand(std::string);
+        void readFiles();
+        bool rename();
+        std::string extractString(std::string);
+        std::string getFile();
+        std::string getOldName();
+        std::string getNewName();
+        bool validNewName();
+        void setFile(std::string);
+        int getNum(std::string);
+        bool isCoreCommand();
+        bool checkinVector(std::string, std::vector <std::string>&);
+        ~Command() = default;
+};
+#endif
